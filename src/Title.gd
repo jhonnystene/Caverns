3a04aca51bf1963341927ffde29dd3df19d77bf7
Johnny Stene <jhonnystene@gmail.com>
2020-08-12 17:18:14 -0600

    Made title screen less BAD

--- src/Title.gd
+++ src/Title.gd
@@ -1,10 +1,9 @@
-extends Control
+extends Node2D
 
-func _on_PlayButton_pressed():
-	get_tree().change_scene("res://scenes/GameWorld.tscn")
+func _on_PlayArea_body_entered(body):
+	if("Player" in body.name):
+		get_tree().change_scene("res://scenes/GameWorld.tscn")
 
-func _on_CreditsButton_pressed():
-	get_tree().change_scene("res://scenes/Credits.tscn")
-
-func _on_ExitButton_pressed():
-	get_tree().quit()
+func _on_CreditsArea_body_entered(body):
+	if("Player" in body.name):
+		get_tree().change_scene("res://scenes/Credits.tscn")
